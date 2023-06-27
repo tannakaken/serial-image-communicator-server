d7047e3177a6721b96c31b467014b6c401e25289
Tanaka Kensaku <tannakaken@gmail.com>
2023-06-27 18:20:49 +0900

    cors対応

--- index.ts
+++ index.ts
@@ -20,7 +20,22 @@ const PORT = 5000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Authorization, access_token'
+    )
+  
+    // intercept OPTIONS method
+    if ('OPTIONS' === req.method) {
+      res.send(200)
+    } else {
+      next()
+    }
+  });
+  
 app.get('/', async (_req: Request, res: Response) => {
     let str = "";
     let metadataWaiting = true;
